6023d981b07974ef83855a1160a0e4c565ed8e58
Miles Pilchik <104800125+Carny2575@users.noreply.github.com>
2024-09-06 11:21:23 -0400

    Changed Text Color for Name Input

---
+++ Lies-and-Lyles-main/Lies-and-Lyles-main/Lies and Lyles/Liar 1-5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lies_and_Lyles
+{
+    //MILES PILCHIK LAST UPDATED 9/5
+    //PURPOSE: LEVEL ONE LIAR 
+    internal class Liar_1_5
+    {
+    }
+}
---
+++ Lies-and-Lyles-main/Lies-and-Lyles-main/Lies and Lyles/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lies_and_Lyles
+{
+
+    // MILES PILCHIK: LAST UPDATED 9/5: CREATED CLASS
+    //PURPOSE: VALIDATION
+    internal class Validation
+    {
+        public static void clearScreen()
+        {
+            Console.WriteLine("Hit enter to clear the screen at any time");
+            Console.ReadLine();
+            Console.Clear();
+           
+        }
+
+
+    }
+}
